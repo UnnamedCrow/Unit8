acef5b3ed713dc9ef9a1e19eee99b64cbf6fb003
Максим Самисько <masechk@gmail.com>
2023-03-30 13:55:59 +0300

    Unit8 - Task1: Program is normal working

--- Unit8/Program.cs
+++ Unit8/Program.cs
@@ -1,15 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Unit8
+namespace Task1
 {
     internal class Program
     {
+
+        static void CleanDirectory(string Path)
+        {
+            string[] subDirectories = Directory.GetDirectories(Path);
+            string[] files = Directory.GetFiles(Path);
+            
+            /// Deleting long time used files
+            foreach (string s in files)
+            {
+                var a = DateTime.Now - File.GetLastAccessTime(s);
+                if (a > TimeSpan.FromMinutes(30))
+                {
+                    File.Delete(s);
+                }
+
+            }
+
+            /// Recursive go around directions tree
+            foreach (string s in subDirectories)
+            {
+                if (Directory.Exists(s))
+                {
+                    if (Directory.GetFileSystemEntries(s).Length == 0)
+                        Directory.Delete(s);
+                    else
+                        CleanDirectory(s);
+                }
+            }
+            Console.WriteLine("Long time used directories and files deleted!");
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Input directories path");
+            string Path = Console.ReadLine();
+            if (Directory.Exists(Path))
+            {
+                try
+                {
+                    CleanDirectory(Path);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
